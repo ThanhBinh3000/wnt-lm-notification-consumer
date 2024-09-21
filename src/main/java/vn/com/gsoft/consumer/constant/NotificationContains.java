314f8e98e3d85ad361a8e4dbc8135fadcb2e40ae
vuductinh <49410355+vuductinh@users.noreply.github.com>
2024-09-21 11:53:02 +0700

    Update NotificationContains.java

--- src/main/java/vn/com/gsoft/consumer/constant/NotificationContains.java
+++ src/main/java/vn/com/gsoft/consumer/constant/NotificationContains.java
@@ -4,4 +4,5 @@ public class NotificationContains {
     public static  final int THONG_BAO_LIEN_MINH = 6;
     public static  final int YEU_CAU_THONG_TIN = 7;
     public static  final int PHAN_HOI_THONG_TIN = 8;
+
 }
